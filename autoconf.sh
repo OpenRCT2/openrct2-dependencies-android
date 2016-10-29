942955d517ba1c4d400e3cb6681b4c1bbb6adaa7
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-10-29 21:31:43 +0200

    Build speexdsp

---
+++ autoconf.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+MAKE="$1"
+TOOLCHAIN_TRIPLE="$2"
+ANDROID_ARCH_NAME="$3"
+ANDROID_PLATFORM_LEVEL="$4"
+PREFIX="$5"
+
+PWD=`pwd`
+
+echo "TOOLCHAIN_TRIPLE: $TOOLCHAIN_TRIPLE"
+echo "ANDROID_PLATFORM_LEVEL: $ANDROID_PLATFORM_LEVEL"
+echo "PREFIX: $PREFIX"
+echo "PWD: $PWD"
+
+MY_NDK="$PWD/../android-toolchain"
+
+export PATH=${ANDROID_TOOLCHAIN_ROOT}/bin:$PATH
+
+/usr/local/opt/android-sdk/ndk-bundle/build/tools/make_standalone_toolchain.py \
+    --arch $ANDROID_ARCH_NAME \
+    --api $ANDROID_PLATFORM_LEVEL \
+    --install-dir "${MY_NDK}"
+
+SYSROOT="${MY_NDK}/sysroot"
+export PATH="${MY_NDK}/bin":$PATH
+
+$(pwd)/autogen.sh
+$(pwd)/configure --host=$TOOLCHAIN_TRIPLE --with-sysroot=${SYSROOT} --prefix=$PREFIX --disable-neon
+
+${MAKE}
+${MAKE} install
